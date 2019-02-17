f22f7c2dae7ceea30f9e65dfe24a06c16178adeb
FumiyaShibusawa <shizai.asdfghjkl@gmail.com>
2019-02-17 20:47:26 +0900

    feat: add if condition function

---
+++ if_condition/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void guessNumber(int number)
+{
+    if (number == 555)
+    {
+        printf("Your number %d: %s\n", number, "Correct. You guessed it!");
+    }
+    else if (number > 555)
+    {
+        printf("Your number %d: %s\n", number, "You guessed too high!");
+    }
+    if (number < 555)
+    {
+        printf("Your number %d: %s\n", number, "You guessed too low!");
+    }
+}
+
+// TODO: command line からの引数（整数）を評価して guessNumber() に渡す方法を見つける
+int main()
+{
+    guessNumber(500);
+    guessNumber(560);
+    guessNumber(555);
+    return 0;
+}
