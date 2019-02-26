db80ec3a4f16f0012610e4b075d5e04aea184c10
FumiyaShibusawa <shizai.asdfghjkl@gmail.com>
2019-02-27 01:07:05 +0900

    chore: add result as comment out

--- array_and_pointers/main.c
+++ array_and_pointers/main.c
@@ -12,6 +12,12 @@ int main()
                i, &vowels[i],
                i, pvowels + i,
                i, vowels + i);
+        // $ ./main
+        // &vowels[0]: aeiou, pvowels + 0: aeiou, vowels + 0: aeiou
+        // &vowels[1]: eiou, pvowels + 1: eiou, vowels + 1: eiou
+        // &vowels[2]: iou, pvowels + 2: iou, vowels + 2: iou
+        // &vowels[3]: ou, pvowels + 3: ou, vowels + 3: ou
+        // &vowels[4]: u, pvowels + 4: u, vowels + 4: u
     }
 
     return 0;
